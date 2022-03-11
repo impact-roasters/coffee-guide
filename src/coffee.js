a0974302a72f5999e1f156d6cbe0087d675ea031
DEJENERUTA <45858936+DEJENERUTA@users.noreply.github.com>
2022-03-11 12:55:21 +0100

    create json file for coffee guide

---
+++ src/coffee.js
@@ -0,0 +1,46 @@
+const coffee = [
+	{
+		name: "Desta",
+		tastingProfile: ["internse sweetness", "floral", "nougat aroma"],
+		processing: ["washed", "sundried"],
+		roastProfile: ["dark"],
+		preparation: [
+			"espresso",
+			"cafe latte",
+			"cappuccino",
+			"cortado",
+			"flat white",
+		],
+		origin: [
+			{
+				name: "Aroresa",
+				latitude: 0,
+				longitude: 0,
+				altitude: 0,
+			},
+			{
+				name: "Jimma Agaro",
+				latitude: 0,
+				longitude: 0,
+				altitude: 0,
+			},
+		],
+		qGrade: 87,
+	},
+	{
+		name: "Desta",
+	},
+	{
+		name: "Desta",
+	},
+	{
+		name: "Desta",
+	},
+	{
+		name: "Desta",
+	},
+	{
+		name: "Desta",
+	},
+];
+export default coffee;
