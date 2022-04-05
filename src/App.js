6c13d4cd4e85ea11668c0bd2c25fea693726af0f
DEJENERUTA <45858936+DEJENERUTA@users.noreply.github.com>
2022-04-05 21:39:25 +0200

    separete props and default value, remove unnecessary classname

--- src/App.js
+++ src/App.js
@@ -14,6 +14,63 @@ const App = () => {
     onToggleCharacteristic,
     onSetRoastLevel,
   } = useAnswers();
+  [
+    {
+      name: "aynu",
+      image: "./assets/aynu.png",
+      qr: 88,
+    },
+    {
+      name: "beza",
+      image: "./assets/beza.png",
+      rating: 92,
+    },
+    {
+      name: "desta",
+      image: "./assets/desta.png",
+      rating: 87,
+    },
+    {
+      name: "kafa",
+      image: "./assets/kafa.png",
+      rating: 89,
+    },
+    {
+      name: "lomi",
+      image: "./assets/lomi.png",
+      rating: 88,
+    },
+    {
+      name: "maaza",
+      image: "./assets/maaza.png",
+      rating: 5,
+    },
+    {
+      name: "nehi",
+      image: "./assets/nehi.png",
+      rating: 87,
+    },
+    {
+      name: "saba",
+      image: "./assets/saba.png",
+      rating: 87,
+    },
+    {
+      name: "worke",
+      image: "./assets/worke.png",
+      rating: 88,
+    },
+    {
+      name: "zala",
+      image: "./assets/zala.png",
+      rating: 87,
+    },
+    {
+      name: "zenu",
+      image: "./assets/zenu.png",
+      rating: 82,
+    },
+  ];
 
   return (
     <div className="app">
@@ -88,6 +145,18 @@ const App = () => {
           }
         />
       </Routes>
+      <div>
+        <h3>CoffeeMatch</h3>
+        <div className="coffee-list">
+          {items.map((item) => (
+            <div className="coffee-item">
+              <img src={item.image} alt={item.name} />
+              <h2>{item.name}</h2>
+              <h3>{item.qr}</h3>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
--- src/CoffeeMatch.js
+++ src/CoffeeMatch.js
@@ -3,67 +3,17 @@ import React from "react";
 const CoffeeMatch = () => {
   [
     {
-      name: "aynu",
-      image: "./assets/aynu.png",
-      qr: 88,
-    },
-    {
-      name: "beza",
-      image: "./assets/beza.png",
-      rating: 92,
-    },
-    {
-      name: "desta",
-      image: "./assets/desta.png",
-      rating: 87,
-    },
-    {
-      name: "kafa",
-      image: "./assets/kafa.png",
-      rating: 89,
-    },
-    {
-      name: "lomi",
-      image: "./assets/lomi.png",
-      rating: 88,
-    },
-    {
-      name: "maaza",
-      image: "./assets/maaza.png",
-      rating: 5,
-    },
-    {
-      name: "nehi",
-      image: "./assets/nehi.png",
-      rating: 87,
-    },
-    {
-      name: "saba",
-      image: "./assets/saba.png",
-      rating: 87,
-    },
-    {
-      name: "worke",
-      image: "./assets/worke.png",
-      rating: 88,
-    },
-    {
-      name: "zala",
-      image: "./assets/zala.png",
-      rating: 87,
-    },
-    {
-      name: "zenu",
-      image: "./assets/zenu.png",
-      rating: 82,
+      name,
+      image,
+      qr,
     },
   ];
   return (
     <div>
       <h3>CoffeeMatch</h3>
-      <div className="coffee-list">
+      <div>
         {items.map((item) => (
-          <div className="coffee-item">
+          <div>
             <img src={item.image} alt={item.name} />
             <h2>{item.name}</h2>
             <h3>{item.qr}</h3>
@@ -75,5 +25,3 @@ const CoffeeMatch = () => {
 };
 
 export default CoffeeMatch;
-
-//get a list of items as a props and render them with their name and picture and link to impact roasters website
