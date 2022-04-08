00ea53062603c6e80755a6cc34b93112af32b9f9
Bence A. Tóth <tothab@gmail.com>
2022-04-08 20:03:54 +0200

    Did some renaming

--- src/useAnswers.js
+++ src/useAnswers.js
@@ -1,7 +1,7 @@
 import { useState, useEffect, useCallback } from "react";
 
 const useAnswers = () => {
-  const [coffeeProfile, onSetCoffeeProfile] = useState(
+  const [profile, onSetProfile] = useState(
     localStorage.getItem("coffeeProfile") ?? null
   );
   const [acidity, onSetAcidity] = useState(
@@ -15,10 +15,10 @@ const useAnswers = () => {
   );
 
   useEffect(() => {
-    if (coffeeProfile !== null) {
-      localStorage.setItem("coffeeProfile", coffeeProfile);
+    if (profile !== null) {
+      localStorage.setItem("coffeeProfile", profile);
     }
-  }, [coffeeProfile]);
+  }, [profile]);
 
   useEffect(() => {
     if (acidity !== null) {
@@ -53,11 +53,11 @@ const useAnswers = () => {
   }, [roastLevel]);
 
   return {
-    coffeeProfile,
+    profile,
     acidity,
     characteristics,
     roastLevel,
-    onSetCoffeeProfile,
+    onSetProfile,
     onSetAcidity,
     onToggleCharacteristic,
     onSetRoastLevel,
