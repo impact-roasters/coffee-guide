bb10eb32a0f009ec1a9eb7aa5a9cc3dbc1e7efd1
Bence A. Tóth <tothab@gmail.com>
2022-09-02 21:11:51 +0200

    Added images and small attributes to options

--- src/components/App/useSteps.js
+++ src/components/App/useSteps.js
@@ -4,47 +4,138 @@ import coffeeData from "data/coffee";
 
 const options = [
   // Profile
-  { type: "profile", value: "clean", label: "Clean (light body)" },
-  { type: "profile", value: "rich", label: "Rich (full body)" },
+  {
+    type: "profile",
+    value: "clean",
+    label: "Clean (light body)",
+    image: "https://placekitten.com/75/60",
+  },
+  {
+    type: "profile",
+    value: "rich",
+    label: "Rich (full body)",
+    image: "https://placekitten.com/75/60",
+  },
 
   // Acidity
-  { type: "acidity", value: "sweet", label: "Sweet" },
-  { type: "acidity", value: "acidic", label: "Acidic" },
-  { type: "acidity", value: "round", label: "Round" },
+  {
+    type: "acidity",
+    value: "sweet",
+    label: "Sweet",
+    image: "https://placekitten.com/75/60",
+    isSmall: true,
+  },
+  {
+    type: "acidity",
+    value: "acidic",
+    label: "Acidic",
+    image: "https://placekitten.com/75/60",
+    isSmall: true,
+  },
+  {
+    type: "acidity",
+    value: "round",
+    label: "Round",
+    image: "https://placekitten.com/75/60",
+  },
 
   // Characteristics
   {
     type: "characteristic",
     value: "chocolate",
     label: "Chocolate aroma",
+    image: "https://placekitten.com/75/60",
+    isSmall: true,
   },
-  { type: "characteristic", value: "citrus", label: "Citrus" },
+  {
+    type: "characteristic",
+    value: "citrus",
+    label: "Citrus",
+    image: "https://placekitten.com/75/60",
+    isSmall: true,
+  },
   {
     type: "characteristic",
     value: "delicate sweetness",
     label: "Delicate sweetness",
+    image: "https://placekitten.com/75/60",
   },
-  { type: "characteristic", value: "floral", label: "Floral aroma" },
+  {
+    type: "characteristic",
+    value: "floral",
+    label: "Floral aroma",
+    image: "https://placekitten.com/75/60",
+    isSmall: true,
+  },
   {
     type: "characteristic",
     value: "forest berry",
     label: "Forest berry aroma",
+    image: "https://placekitten.com/75/60",
+    isSmall: true,
   },
-  { type: "characteristic", value: "fruity", label: "Fruity" },
-  { type: "characteristic", value: "honey", label: "Honey aroma" },
+  {
+    type: "characteristic",
+    value: "fruity",
+    label: "Fruity",
+    image: "https://placekitten.com/75/60",
+    isSmall: true,
+  },
+  {
+    type: "characteristic",
+    value: "honey",
+    label: "Honey aroma",
+    image: "https://placekitten.com/75/60",
+    isSmall: true,
+  },
   {
     type: "characteristic",
     value: "intense sweetness",
     label: "Intense sweetness",
+    image: "https://placekitten.com/75/60",
+    isSmall: true,
   },
-  { type: "characteristic", value: "nougat", label: "Nougat aroma" },
-  { type: "characteristic", value: "nut", label: "Nut aroma" },
-  { type: "characteristic", value: "spicy", label: "Spicy aroma" },
+  {
+    type: "characteristic",
+    value: "nougat",
+    label: "Nougat aroma",
+    image: "https://placekitten.com/75/60",
+    isSmall: true,
+  },
+  {
+    type: "characteristic",
+    value: "nut",
+    label: "Nut aroma",
+    image: "https://placekitten.com/75/60",
+    isSmall: true,
+  },
+  {
+    type: "characteristic",
+    value: "spicy",
+    label: "Spicy aroma",
+    image: "https://placekitten.com/75/60",
+    isSmall: true,
+  },
 
   // Roast levels
-  { type: "roastLevel", value: "light", label: "Light" },
-  { type: "roastLevel", value: "medium", label: "Medium" },
-  { type: "roastLevel", value: "dark", label: "Dark" },
+  {
+    type: "roastLevel",
+    value: "light",
+    label: "Light",
+    image: "https://placekitten.com/75/60",
+  },
+  {
+    type: "roastLevel",
+    value: "medium",
+    label: "Medium",
+    image: "https://placekitten.com/75/60",
+  },
+  {
+    type: "roastLevel",
+    value: "dark",
+    label: "Dark",
+    image: "https://placekitten.com/75/60",
+  },
 ];
 
 const useSteps = ({
