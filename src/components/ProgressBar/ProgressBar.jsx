23eda32acdf1f5ab9353dd95e12784d11dc07f0f
Bence A. Tóth <tothab@gmail.com>
2022-11-27 14:26:24 +0100

    Progress bar styles

--- src/components/ProgressBar/ProgressBar.jsx
+++ src/components/ProgressBar/ProgressBar.jsx
@@ -1,6 +1,20 @@
+import "./ProgressBar.css";
+
 const ProgressBar = ({ stepIndex, steps }) => (
-  <div>
-    {stepIndex + 1} / {steps.length}
+  <div className="progress-steps-wrapper">
+    <div className="progress-steps">
+      {steps.map((step) => (
+        <div key={step.route} className="progress-step">
+          {step.title}
+        </div>
+      ))}
+    </div>
+    <div className="progress-bar">
+      <div
+        className="progress-bar-indicator"
+        style={{ width: `${((stepIndex + 1) / steps.length) * 100}%` }}
+      />
+    </div>
   </div>
 );
 
