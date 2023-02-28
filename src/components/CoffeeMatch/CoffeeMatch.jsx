ac5f9ad1bcf4bf2b4e507e1f74b8c9fd1c345652
DEJENERUTA <dejenedaba269@gmail.com>
2023-02-28 09:54:40 +0100

    Resize coffee image, show 3 matches and link to impact roasters page

--- src/components/CoffeeMatch/CoffeeMatch.jsx
+++ src/components/CoffeeMatch/CoffeeMatch.jsx
@@ -1,9 +1,6 @@
 import { useMemo } from "react";
-
 import { getMatches } from "./utility";
-
 import "./CoffeeMatch.css";
-
 import NavigationButton from "components/NavigationButton/NavigationButton";
 
 const CoffeeMatch = ({ answers }) => {
@@ -20,20 +17,42 @@ const CoffeeMatch = ({ answers }) => {
     [acidity, characteristics, profile, roastLevel]
   );
 
+  const primaryMatch = matches[0]; // primary match is the first item in the array
+  const otherMatches = matches.slice(1, 4); // up to 3 additional matches
+
   return (
     <div className="match">
-      <h3 className="coffee-match-title">Here is your Coffee match!</h3>
+      <h3 className="coffee-match-title">Here are your Coffee matches!</h3>
       <div className="match-content">
-        {matches.map((item) => (
-          <div className="items" key={item.name}>
+        <div className="items" key={primaryMatch.name}>
+          <a href={primaryMatch.website} target="_blank" rel="noreferrer">
             <img
               className="coffee-beans-pic"
-              src={item.image}
-              alt={item.name}
+              src={primaryMatch.image}
+              alt={primaryMatch.name}
             />
-            <h2>{item.name}</h2>
-            <h3>{item.qGrade}</h3>
-            <h3>{item.characteristicMatches}</h3>
+            <h2>{primaryMatch.name}</h2>
+            <h3>{primaryMatch.qGrade}</h3>
+            <h3>{primaryMatch.characteristicMatches}</h3>
+          </a>
+        </div>
+        {otherMatches.map((item) => (
+          <div className="items" key={item.name}>
+            <a
+              href="https://impactroasters.dk/collections/roasted-coffee-beans?"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              View more about {item.name}
+              <img
+                className="coffee-beans-pic"
+                src={item.image}
+                alt={item.name}
+              />
+              <h2>{item.name}</h2>
+              <h3>{item.qGrade}</h3>
+              <h3>{item.characteristicMatches}</h3>
+            </a>
           </div>
         ))}
       </div>
