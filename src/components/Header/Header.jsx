fee5828087a333f85e7f05a03afcd3c6d06c33b7
DEJENERUTA <dejenedaba269@gmail.com>
2023-03-03 09:33:47 +0100

    Add IR logo and language switcher to Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,20 @@
 import "./Header.css";
+import IRLogo from "../../data/images/IRLogo.png";
 
 const Header = () => (
-  <header className="app-header">Header will come here</header>
+  <header className="app-header">
+    <div className="logo">
+      <a href="https://impactroasters.dk/">
+        <img src={IRLogo} alt="IR Logo" />
+      </a>
+    </div>
+    <div className="language-switcher">
+      <select>
+        <option value="da">DA</option>
+        <option value="en">EN</option>
+      </select>
+    </div>
+  </header>
 );
 
 export default Header;
