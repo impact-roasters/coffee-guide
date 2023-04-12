b47a9c1dfaa61070ca0e980c14abce9ac8a804ef
Bence A. Tóth <tothab@gmail.com>
2023-04-12 20:00:02 +0200

    Switched from flag images to emoji

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,6 @@
 import useLocale from "components/App/useLocale";
 
 import IRLogo from "../../data/images/IRLogo.png";
-import Danflag from "../../data/images/Danflag.png";
-import Engflag from "../../data/images/Engflag.png";
 
 import "./Header.css";
 
@@ -20,12 +18,20 @@ const Header = ({ setLocaleToEnglish, setLocaleToDanish }) => {
       </div>
       <div className="language-switcher">
         {locale === "en-us" ? (
-          <button className="lang-button" onClick={setLocaleToDanish}>
-            <img className="flag" src={Danflag} alt="Danish flag" />
+          <button
+            aria-label="Dansk"
+            className="lang-button"
+            onClick={setLocaleToDanish}
+          >
+            🇩🇰
           </button>
         ) : (
-          <button className="lang-button" onClick={setLocaleToEnglish}>
-            <img className="flag-eng" src={Engflag} alt="English flag" />
+          <button
+            aria-label="English"
+            className="lang-button"
+            onClick={setLocaleToEnglish}
+          >
+            🇬🇧
           </button>
         )}
       </div>
